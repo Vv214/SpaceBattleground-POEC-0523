e34781ea2335891a29b996d751edb35968e9d6e0
Vv214 <vrf.lbc@gmail.com>
2023-07-26 16:42:46 +0200

    Modify space.sql

--- database/space.sql
+++ database/space.sql
@@ -2,6 +2,15 @@
 
 CREATE DATABASE IF NOT EXISTS `space`;
 
+CREATE USER
+    If not EXISTS 'admin' @'localhost' IDENTIFIED
+WITH
+    mysql_native_password BY '';
+
+GRANT ALL PRIVILEGES ON space.* TO 'admin'@'localhost';
+
+FLUSH PRIVILEGES;
+
 Use `space`;
 
 SELECT * FROM player;
