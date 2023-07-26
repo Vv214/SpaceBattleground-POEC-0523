732da9aadf0f2402a7dbac5221c1c76826d08220
ROYPIDO <roypido@gmail.com>
2023-07-26 11:54:53 +0200

    CRUD Ressource is OK

--- src/main/java/com/templateproject/api/repository/RessourceRepository.java
+++ src/main/java/com/templateproject/api/repository/RessourceRepository.java
@@ -1,12 +1,15 @@
 package com.templateproject.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.templateproject.api.entity.Ressource;
 
-@Repository
 public interface RessourceRepository extends JpaRepository<Ressource, Integer> {
+
+
+    Ressource findByName(String name);
+
+
 
 }
 
