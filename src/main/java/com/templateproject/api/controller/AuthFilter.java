56e6bb2adc5645d28ea35758062a7fadb17db188
Marco Hanquier <marco.hanquier@hotmail.fr>
2023-08-03 12:05:03 +0200

    test

--- src/main/java/com/templateproject/api/controller/AuthFilter.java
+++ src/main/java/com/templateproject/api/controller/AuthFilter.java
@@ -1,35 +1,35 @@
-// package com.templateproject.api.controller;
+package com.templateproject.api.controller;
 
-// import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Component;
 
-// import com.templateproject.api.service.AuthService;
+import com.templateproject.api.service.AuthService;
 
-// import jakarta.servlet.*;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-// import java.io.IOException;
+import java.io.IOException;
 
-// @Component
-// public class AuthFilter implements Filter {
+@Component
+public class AuthFilter implements Filter {
 
-//     private final AuthService authService;
+    private final AuthService authService;
 
-//     AuthFilter(AuthService authService) {
-//         this.authService = authService;
-//     }
+    AuthFilter(AuthService authService) {
+        this.authService = authService;
+    }
 
-//     @Override
-//     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
-//             throws IOException, ServletException {
-//         var request = (HttpServletRequest) servletRequest;
-//         var response = (HttpServletResponse) servletResponse;
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+            throws IOException, ServletException {
+        var request = (HttpServletRequest) servletRequest;
+        var response = (HttpServletResponse) servletResponse;
 
-//         var token = request.getHeader("x-token");
-//         System.out.println(token + " token back");
+        var token = request.getHeader("x-token");
+        System.out.println(token + " token back");
 
-//         var path = request.getServletPath();
-//         if (!path.equals("/register") && !path.equals("/login")) {
+        var path = request.getServletPath();
+        if (!path.equals("/register") && !path.equals("/login")) {
 
             response.setHeader("Access-Control-Allow-Origin", "*");
 
@@ -38,18 +38,18 @@
             response.setHeader("Access-Control-Allow-Headers",
                     "Origin, Accept, X-Requested-With, Content-Type,Access-Control-Request-Method, Access-Control-Request-Headers");
 
-//             Integer playerID = authService.findUserIdByToken(token);
-//             if (playerID == null) {
-//                 response.setHeader("Content-type", "application/json");
-//                 response.setStatus(402);
-//                 response.getWriter().println("{" +
-//                         "\"message\" : \"Invalid Token\"" +
-//                         "}");
-//                 return;
-//             }
+            Integer playerID = authService.findUserIdByToken(token);
+            if (playerID == null) {
+                response.setHeader("Content-type", "application/json");
+                response.setStatus(402);
+                response.getWriter().println("{" +
+                        "\"message\" : \"Invalid Token\"" +
+                        "}");
+                return;
+            }
 
-//             request.setAttribute("playerID", playerID);
-//         }
-//         chain.doFilter(request, response);
-//     }
-// }
+            request.setAttribute("playerID", playerID);
+        }
+        chain.doFilter(request, response);
+    }
+}
